358b6d21c64ed51e2c801574d64b153e8c5c780b
Arkadeep Das <arkadeep.das8@gmail.com>
2023-05-28 21:59:03 +0530

    add utils and config file

---
+++ Pix2Pix_Gans_Config.py
@@ -0,0 +1,21 @@
+# Config file for Pix2Pix GANs
+import torch
+
+# IF gpu is present then we are going to use GPU else CPU
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+# Dataset paths
+TRAIN_DIR = 'pix2pix_dataset/maps/maps/train'
+VALID_DIR = 'pix2pix_dataset/maps/maps/val'
+# Fixed learning rate
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 16
+NUM_WORKERS = 2
+IMAGE_SIZE = 256
+CHANNEL_IMAGE = 3
+L2_LAMBDA = 100
+NUM_EPOCHS = 100
+LOAD_MODE = False
+SAVE_MODEL = False
+# Checkpoint for Discriminator and Generator
+CHECKPOINT_DISC = 'disc.path.tar'
+CHECKPOINT_GEN = 'gen.path.tar'
--- Pix2Pix_Gans_Train.py
+++ Pix2Pix_Gans_Train.py
@@ -1,6 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import Pix2Pix_Gans_Config
 from Pix2Pix_Gans_Dataset import MapDataset
 from Pix2Pix_GANs_Generator_Unet import Generator
 from Pix2Pix_GANs_Discriminator import Discriminator
+from torch.utils.data import DataLoader
+
+def main():
+    disc = Discriminator(in_channels = 3).to(Pix2Pix_Gans_Config.DEVICE)
+    gen = Generator(in_channels = 3).to(Pix2Pix_Gans_Config.DEVICE)
+    # Beta values are specify in paper
+    optimizer_disc = optim.Adam(disc.parameters(), lr = Pix2Pix_Gans_Config.LEARNING_RATE, betas = (0.5, 0.999))
+    optimizer_gen = optim.Adam(gen.parameters(), lr = Pix2Pix_Gans_Config.LEARNING_RATE, betas = (0.5, 0.999))
+    # Here they are using standard binary cross entropy loss
+    BCE = nn.BCEWithLogitsLoss()
+    # They also use L1_loss or mean absolute error (MAE) loss
+    # Here they don't use W-GAN-GP loss because it doesn't work well
+    L1_LOSS = nn.L1Loss() # Mean absolute error (MAE) loss
+    
